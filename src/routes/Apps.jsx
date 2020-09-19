56ea604f6f2bd1c8744620dc86aff217f4b2cebb
Bruno Caruso <brunofcaruso@gmail.com>
2020-09-19 01:02:06 -0300

    Actualizacion de rutas
    Se actualizaron las rutas para que funcionen cuando se les pasa el ID en la URL

--- src/routes/Apps.jsx
+++ src/routes/Apps.jsx
@@ -1,65 +1,78 @@
 import React from "react";
 import { Header, Grid } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
 
 export default function Apps() {
-  return (
-    <div>
-      <Header as="h2" inverted textAlign="center">
-        All Apps
-      </Header>
-      <Grid>
-        <Grid.Row columns={5}>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row columns={5}>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row columns={5}>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-          <Grid.Column>
-            <p />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </div>
-  );
+  const { id } = useParams();
+
+  if (id) {
+    return (
+      <div>
+        <Header as="h2" inverted textAlign="center">
+          App ID {id}
+        </Header>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Header as="h2" inverted textAlign="center">
+          All Apps
+        </Header>
+        <Grid>
+          <Grid.Row columns={5}>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={5}>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={5}>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+            <Grid.Column>
+              <p />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
+  }
 }
